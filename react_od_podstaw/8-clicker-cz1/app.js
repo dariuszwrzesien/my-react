4870ec45512bb8a3b1b45366c3c2712f43795b37
Dariusz Wrzesień <dariuszwrzesien@gmail.com>
2024-11-07 12:45:34 +0100

    Refactor Counter component to use functional buttons and improve state management

--- react_od_podstaw/8-clicker-cz1/app.js
+++ react_od_podstaw/8-clicker-cz1/app.js
@@ -1,49 +1,103 @@
+const Type = {
+  MINUS: "minus",
+  RESET: "reset",
+  PLUS: "plus",
+};
+
+const Button = ({ label, type, value, click, ...props }) => {
+  return <button onClick={() => click(type, value)}>{label}</button>;
+};
+
+const CounterDisplay = ({ count, result, ...props }) => {
+  const color = result > 0 ? "green" : result < 0 ? "red" : "black";
+  return (
+    <>
+      <h1>click count: {count}</h1>
+      <h1 style={{ color: color }}>result: {result}</h1>
+    </>
+  );
+};
+
 class Counter extends React.Component {
-
-  state = {
-    count: 0,
-    result: this.props.result
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: this.props.count,
+      result: this.props.result,
+    };
   }
 
-  handleMathClick(type, number = 1) {
-    // debugger
-    if (type === "subtraction") {
-      this.setState(prevState => ({
-        count: prevState.count + 1,
-        result: prevState.result - number
-      }))
-    } else if (type === "reset") {
-      this.setState(prevState => (
-        {
-          count: prevState.count + 1,
-          result: this.props.result
-        }
-      ))
-    } else if (type === "addition") {
-      this.setState(prevState => (
-        {
-          count: prevState.count + 1,
-          result: prevState.result + number
-        }
-      ))
+  handleClick = (type = Type.RESET, number = 1) => {
+    switch (type) {
+      case Type.MINUS:
+        this.setState((state) => ({
+          count: state.count + 1,
+          result: state.result - number,
+        }));
+        break;
+      case Type.RESET:
+        this.setState(() => ({
+          count: this.props.count,
+          result: this.props.result,
+        }));
+        break;
+      case Type.PLUS:
+        this.setState((state) => ({
+          count: state.count + 1,
+          result: state.result + number,
+        }));
+        break;
+      default:
+        console.log("error");
     }
+  };
 
-  }
   render() {
     return (
       <>
-        <button onClick={this.handleMathClick.bind(this, "subtraction", 10)}>-10</button>
-        <button onClick={() => this.handleMathClick(1, "subtraction")}>-1</button>
-        <button onClick={this.handleMathClick.bind(this, "reset")}>Reset</button>
-        <button onClick={this.handleMathClick.bind(this, "addition", 1)}>+1</button>
-        <button onClick={this.handleMathClick.bind(this, "addition", 10)}>+10</button>
-        <h1>Liczba kliknięć: {this.state.count}</h1>
-        <h1>Wynik: {this.state.result}</h1>
+        <Button
+          label="-1"
+          type={Type.MINUS}
+          value={1}
+          click={this.handleClick}
+        />
+        <Button
+          label="-10"
+          type={Type.MINUS}
+          value={10}
+          click={this.handleClick}
+        />
+        <Button label="Reset" type={Type.RESET} click={this.handleClick} />
+        <Button
+          label="+10"
+          type={Type.PLUS}
+          value={10}
+          click={this.handleClick}
+        />
+        <Button
+          label="+1"
+          type={Type.PLUS}
+          value={1}
+          click={this.handleClick}
+        />
+        <CounterDisplay count={this.state.count} result={this.state.result} />
+        {/* <button onClick={() => this.handleClick("minus")}>-1</button>
+        <button onClick={() => this.handleClick("minus", 10)}>-10</button>
+        <button onClick={() => this.handleClick()}>Reset</button>
+        <button onClick={() => this.handleClick("plus", 10)}>+10</button>
+        <button onClick={() => this.handleClick("plus")}>+1</button> */}
+        {/* <h1>click count: {this.state.count}</h1>
+        <h1>result: {this.state.result}</h1> */}
       </>
-    )
+    );
   }
-
 }
 
-const startValue = 0;
-ReactDOM.render(<Counter result={startValue} />, document.getElementById('root'))
+const init = {
+  result: 0,
+  count: 0,
+};
+ReactDOM.render(
+  <Counter count={init.count} result={init.result} />,
+  document.getElementById("root")
+);
