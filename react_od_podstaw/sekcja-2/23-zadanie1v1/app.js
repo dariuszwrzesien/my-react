774c0287c1045fd82a6c563766ccd97a2b3e2c0c
Dariusz Wrzesień <dariuszwrzesien@gmail.com>
2024-11-11 23:52:35 +0100

    Refactor Draw and List components to enhance functionality and improve user interaction

--- react_od_podstaw/sekcja-2/23-zadanie1v1/app.js
+++ react_od_podstaw/sekcja-2/23-zadanie1v1/app.js
@@ -7,27 +7,79 @@
 // Wersja na 6 - dodaje do trzech istniejących wróżb kolejną
 
 class Draw extends React.Component {
-
   state = {
-    options: ["1", "2", "3"],
-    option: null
-  }
+    selectedIndex: 0,
+    options: ["wróżba 1", "wróżba 2", "wróżba 3"],
+    form: {
+      optionValue: "",
+    },
+  };
 
   handleShowOption = () => {
     const index = Math.floor(Math.random() * this.state.options.length);
     this.setState({
-      option: this.state.options[index]
-    })
-  }
+      selectedIndex: index,
+    });
+  };
+
+  handleFormOptionValueChange = (e) => {
+    this.setState({
+      form: {
+        optionValue: e.target.value,
+      },
+    });
+  };
+
+  handleAddOption = (e) => {
+    e.preventDefault();
+    const options = [...this.state.options];
+    options.push(this.state.form.optionValue);
+    this.setState(
+      {
+        options,
+        form: {
+          optionValue: "",
+        },
+      },
+      () => this.showInfo()
+    );
+  };
+
+  isValid = () => {
+    if (this.state.form.optionValue === "") {
+      return false;
+    }
+    return true;
+  };
+
+  showInfo = () => {
+    alert(`
+      Wróżba została dodana!
+      Aktualne wróżby: ${this.state.options.join(", ")}
+      `);
+  };
 
   render() {
+    const { selectedIndex, options } = this.state;
     return (
-      <div>
+      <>
         <button onClick={this.handleShowOption}>Zobacz wróżbę</button>
-        {this.state.option ? <h1>{this.state.option}</h1> : null}
-      </div>
-    )
+        <br />
+        <form onSubmit={this.handleAddOption}>
+          <input
+            value={this.state.form.optionValue}
+            onChange={this.handleFormOptionValueChange}
+          />
+          {!this.isValid() && (
+            <p style={{ color: "red" }}>Wpisz coś jeśli chcesz dodać wróżbę.</p>
+          )}
+          <br />
+          <button type="submit">Dodaj wróźbę</button>
+        </form>
+        {selectedIndex !== null && <p>{options[selectedIndex]}</p>}
+      </>
+    );
   }
 }
 
-ReactDOM.render(<Draw />, document.getElementById('root'))
+ReactDOM.render(<Draw />, document.getElementById("root"));
