58507b28d821a7bc4dd0a1c9e6c7c8138fd85e49
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2025-03-12 08:12:42 +0100

    Refactor routing in App component and add Button component; implement navigation in Form and Map components

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
+import City from './components/City';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
+import Form from './components/Form';
 import AppLayout from './pages/AppLayout';
 import Homepage from './pages/Homepage';
 import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import Pricing from './pages/Pricing';
 import Product from './pages/Product';
-import City from './components/City';
 
 function App() {
   const [cities, setCities] = useState([]);
@@ -31,10 +32,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
@@ -44,7 +42,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
--- worldwise/src/components/City.jsx
+++ worldwise/src/components/City.jsx
@@ -1,4 +1,5 @@
 import { useParams, useSearchParams } from 'react-router-dom';
+
 import styles from './City.module.css';
 
 const formatDate = (date) =>
